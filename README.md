abc2d99b03df8f194fcb12e3fc3bf0ad2de1a665
Luídne Mota <luidne@gmail.com>
2018-04-27 23:05:21 -0300

    Arquivos do projeto Maven.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# ifto.telematica.geracaodehorarios
+﻿# Geração de Horários Escolar com grafos
 Cria plugin para o Neo4j. O plugin é uma procedure para obter os melhores horários para uma disciplina. Projeto Java usado no curso de Pós-Graduação em Telemática pelo Instituto Federal de Ciência, Educação e Tecnologia do Tocantins - IFTO.
---
+++ src/test/java/br/edu/ifto/telematica/ManualFullTextIndexTest.java
@@ -0,0 +1,53 @@
+package br.edu.ifto.telematica;
+
+import br.ifto.edu.telematica.FullTextIndex;
+import static org.hamcrest.core.IsEqual.equalTo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.neo4j.driver.v1.AuthTokens;
+import org.neo4j.driver.v1.Config;
+import org.neo4j.driver.v1.Driver;
+import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import static org.neo4j.driver.v1.Values.parameters;
+import org.neo4j.harness.junit.Neo4jRule;
+
+/**
+ *
+ * @author Luídne
+ * Pego em: https://neo4j.com/docs/developer-manual/current/extending-neo4j/procedures/
+ */
+public class ManualFullTextIndexTest {
+    // This rule starts a Neo4j instance
+    @Rule
+    public Neo4jRule neo4j = new Neo4jRule()
+            // This is the Procedure we want to test
+            .withProcedure(FullTextIndex.class);
+    
+    //@Test
+    public void shouldAllowIndexingAndFindingANode() {
+        // In a try-block, to make sure we close the driver after the test
+        try (Driver driver = GraphDatabase.driver("bolt://127.0.0.1:7687", AuthTokens.basic("admin", "admin"))) {
+            // Given I've started Neo4j with the FullTextIndex procedure class
+            //       which my 'neo4j' rule above does.
+            Session session = driver.session();
+            
+            // And given I have a node in the database
+            long nodeId = session.run("CREATE (d:disciplina {name: 'Disc. Ext.'}) RETURN id(d)")
+                    .single()
+                    .get(0)
+                    .asLong();
+            
+            // When I use the index procedure to index a node
+            session.run("CALL br.edu.ifto.telematica.index({id}, ['name'])", parameters("id", nodeId));
+            
+            // Then I can search for that node with lucene query syntax
+            StatementResult result = session.run("CALL br.edu.ifto.telematica.search(('disciplina', 'name: Disc*')");
+            System.out.println("nodeId = "+nodeId);
+            System.out.println("result nodeId = "+result.single().get("nodeId").asLong());
+            assertThat(result.single().get("nodeId").asLong(), equalTo(nodeId));
+        } catch (Exception e) {}
+    }
+}
